8ceaa37ec1692e7708a1350726ae03c12f52bdbb
Sumit <johnnygirwal@gmail.com>
2023-02-05 16:26:24 +0530

    Added google login signup and updated login signup pages

--- backend/ImaGen/ImaGen/settings.py
+++ backend/ImaGen/ImaGen/settings.py
@@ -58,7 +58,44 @@ INSTALLED_APPS = [
     # Application
     'imagenapp',
     'accounts',
+
+    # Google all auth
+    'django.contrib.sites', # must
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
+
+
+# auth google settings
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
--- backend/ImaGen/ImaGen/urls.py
+++ backend/ImaGen/ImaGen/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('imagenapp.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
      
     
 ] 
